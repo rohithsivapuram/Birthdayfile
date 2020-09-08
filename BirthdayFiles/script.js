fcaae70deb01903dcec57c1662b8b95a2ca7461c
rohithsivapuram <67912428+rohithsivapuram@users.noreply.github.com>
2020-09-08 21:34:42 +0530

    uploded fifle

---
+++ BirthdayFiles/script.js
@@ -0,0 +1,10 @@
+var a =prompt("Enter User name");
+var b = prompt("Enter password")
+
+if (a== "preethi" && b=="birthday" ) {
+	window.location.href = "start.html";
+}
+else {
+	alert("invalid details");
+	window.location.href = "index.html";
+}
